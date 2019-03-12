558548b4d915efa0c5d77bf24f11e5631e651dc9
pishuiwei <823598326@qq.cpm>
2019-03-13 00:34:04 +0800

    remove

--- CNote/src/main.c
+++ CNote/src/main.c
@@ -1,24 +1,8 @@
 #include <stdio.h>
-#include <errno.h>
-#include <string.h>
 
-//extern int errno ;
+int main () {
+    extern int i;// 赋初值是在定义时进行，外部变量声明不能赋初值
 
-int main ()
-{
-    FILE * pf;
-    int errnum;
-    pf = fopen ("unexist.txt", "rb");
-    if (pf == NULL)
-    {
-        errnum = errno;
-        fprintf(stderr, "错误号: %d\n", errno);
-        perror("通过 perror 输出错误");
-        fprintf(stderr, "打开文件错误: %s\n", strerror( errnum ));
-    }
-    else
-    {
-        fclose (pf);
-    }
+    printf("%d", i);
     return 0;
 }
